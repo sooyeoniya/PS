1f9f1f2d9fbe03ddbb73d600902b3042bb6b613b
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2023-11-10 21:30:00 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 52 ms, Memory: 5540 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.cc
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.cc
@@ -6,12 +6,12 @@ string name[100000];
 int power[100000];
 
 int binary_search(int x) { // https://cjh5414.github.io/binary-search/
-	int left = 0, right = n - 1, mid = 0;
+	int low = 0, high = n - 1, mid = 0;
 
-	while (left <= right) {
-		mid = (left + right) / 2;
-		if (x <= power[mid]) right = mid - 1;
-		else left = mid + 1;
+	while (low <= high) {
+		mid = (low + high) / 2;
+		if (x <= power[mid]) high = mid - 1;
+		else low = mid + 1;
 	}
 	if (x > power[mid]) return mid + 1;
 	else return mid;
