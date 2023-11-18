70531617eb1c2e3192594b32c7e7fa8451ceac77
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2023-11-19 01:27:16 +0900

    Update Four Squares.cc

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int n, dp[50001];
+int n, dp[50001]; // 1 <= n <= 50000
 
 int main(void)
 {
@@ -12,11 +12,15 @@ int main(void)
 
    for (int i = 1; i <= n; i++)
    {
-      dp[i] = i;
-      for (int j = 1; j * j <= i; j++)
+      dp[i] = i; // 1부터 n까지의 수에 대해 dp 배열 초기화, 각 수는 최소한 그 수만큼의 1의 제곱으로 나타낼 수 있기 때문
+      // 각 i에 대하여 가능한 모든 제곱수 j*j를 뺀 값에 대한 dp값(해당 값에서의 제곱수의 최소 개수) 조회
+      for (int j = 1; j * j <= i; j++) // 
       {
+         // 현재 dp[i]와 비교하여 더 작은 값으로 설정
+         // "dp[i - j * j] + 1"에서 1을 더해주는 이유는 현재 뺀 제곱수(j*j)의 경우의 수를 하나 더해준 것
          dp[i] = min(dp[i], dp[i - j * j] + 1);
       }
    }
+   // 합이 자연수 n과 같게 되는 제곱수들의 최소 개수 출력
    cout << dp[n] << "\n";
 }
