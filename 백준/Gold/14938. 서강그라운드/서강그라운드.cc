8b1cbc3310496f2fbb621e0e1e933082745cdee2
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2023-12-18 20:52:42 +0900

    Update 서강그라운드.cc

--- 백준/Gold/14938. 서강그라운드/서강그라운드.cc
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.cc
@@ -1,3 +1,15 @@
+/**
+* 풀이 시간: 2:15:06
+* 시간 복잡도: O(n^3 + n^2) = O(n^3) // floyd_warshall()의 3중 for문 + main()의 2중 for문
+* 공간 복잡도: O(n^2 + n) = O(n^2) // dist 공간 복잡도 O(n^2) + item 공간 복잡도 O(n)
+* 참고 자료: https://velog.io/@yyj8771/Python-백준-14938번-서강그라운드
+* [오류 원인]
+* 1. 초기화 문제: 모든 거리의 값(dist)이 0으로 초기화 되어 있는 부분 오류
+* 		 floyd_warshall 알고리즘에서는 자신으로부터 자신까지의 거리는 0
+* 		 그 외의 거리는 무한대(INF)로 초기화해야 함
+* 2. 인덱스 문제: 오류의 원인은 인덱스가 0부터 시작하고 있기 때문
+* 		 지역의 번호는 1이상 n이하의 정수여야 함
+*/
 #include <iostream>
 #include <vector>
 using namespace std;
