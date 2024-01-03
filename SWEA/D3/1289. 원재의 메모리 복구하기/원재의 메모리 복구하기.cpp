c59b5a6300b7c5fcadf19ca7f2017e12d8062b5c
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2024-01-03 14:18:26 +0900

    Update 원재의 메모리 복구하기.cpp

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.cpp
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.cpp
@@ -1,3 +1,6 @@
+// 풀이 시간: 1시간 - 처음에 bit 값을 그대로 받으려다가 string 라이브러리로 바꾸는 과정에서 시간 소요
+// 시간 복잡도: O(T*N) - N은 str 문자열의 길이
+// 공간 복잡도: O(1) - 전부 상수 값이므로
 #include <iostream>
 #include <string>
 using namespace std;
@@ -20,7 +23,7 @@ int main(void)
             }
             else {
                 if (flag == 0 & str[j] == '1') { flag = 1; cnt++; }
-                else if (flag == 1 & str[j] == '0') { flag = 0; cnt++; }
+                if (flag == 1 & str[j] == '0') { flag = 0; cnt++; }
             }
         }
         cout << "#" << i << " " << cnt << "\n";
