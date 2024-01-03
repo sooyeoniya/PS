70d7979264cd39b6f52822697380ee0af2fc215a
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2024-01-03 14:09:23 +0900

    Update 원재의 메모리 복구하기.cpp

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.cpp
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.cpp
@@ -9,35 +9,18 @@ int main(void)
     cout.tie(0);
     int T;
     cin >> T;
-    for (int i = 1; i <= T; i++)
-    {
+    for (int i = 1; i <= T; i++) {
         string str;
         int cnt = 0, flag = 0;
         cin >> str;
-        for (int j = 0; j < str.length(); j++)
-        {
-            if (j == 0)
-            {
-                if (str[j] == '1')
-                {
-                    flag = 1;
-                    cnt++;
-                }
-                else
-                    continue;
+        for (int j = 0; j < str.length(); j++) {
+            if (j == 0) {
+                if (str[j] == '1') { flag = 1; cnt++; }
+                else continue;
             }
-            else
-            {
-                if (flag == 0 & str[j] == '1')
-                {
-                    flag = 1;
-                    cnt++;
-                }
-                else if (flag == 1 & str[j] == '0')
-                {
-                    flag = 0;
-                    cnt++;
-                }
+            else {
+                if (flag == 0 & str[j] == '1') { flag = 1; cnt++; }
+                else if (flag == 1 & str[j] == '0') { flag = 0; cnt++; }
             }
         }
         cout << "#" << i << " " << cnt << "\n";
