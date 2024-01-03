1e6f1962d4d426f530d444a8f218a028b1852a2b
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2024-01-03 13:48:53 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 5 ms, Memory: 13,540 KB -BaekjoonHub

---
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    cin >> T;
+    for (int i = 1; i <= T; i++)
+    {
+        string str;
+        int cnt = 0, flag = 0;
+        cin >> str;
+        for (int j = 0; j < str.length(); j++)
+        {
+            if (j == 0)
+            {
+                if (str[j] == '1')
+                {
+                    flag = 1;
+                    cnt++;
+                }
+                else
+                    continue;
+            }
+            else
+            {
+                if (flag == 0 & str[j] == '1')
+                {
+                    flag = 1;
+                    cnt++;
+                }
+                else if (flag == 1 & str[j] == '0')
+                {
+                    flag = 0;
+                    cnt++;
+                }
+            }
+        }
+        cout << "#" << i << " " << cnt << "\n";
+    }
+}
