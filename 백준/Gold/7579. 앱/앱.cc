542ac8ca8e781f1ef720af8921c97d9bb7376184
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-01-27 22:23:12 +0900

    Update 앱.cc

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,41 +1,29 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <cstring>
+#include <string>
 using namespace std;
-
-int N, M;
-int m[101] = { 0, };
-int c[101] = { 0, };
+int N, M, ans = 1e9;
+int m[101] = { 0, }, c[101] = { 0, };
 int dp[101][10001] = { 0, };
-int result = 1000000000;
-
 
 int main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(0); cout.tie(0);
+   cin >> N >> M;
+   for (int i = 1; i <= N; ++i) cin >> m[i];
+   for (int i = 1; i <= N; ++i) cin >> c[i];
 
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
+   for (int i = 1; i <= N; ++i) { // 앱
+      for (int j = 0; j <= 10000; ++j) { // 비용
+         if (c[i] <= j) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - c[i]] + m[i]);
+         else dp[i][j] = dp[i - 1][j];
+         if (dp[i][j] >= M) ans = min(ans, j);
+         // 메모리가 M 이상 되었을 때 최소 비용 갱신
+      }
+   }
 
-	cin >> N >> M;
-
-	for (int i = 1; i <= N; i++){
-		cin >> m[i];
-	}
-	for (int i = 1; i <= N; i++) {
-		cin >> c[i];
-	}
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j <= 10000; j++) {
-			if (c[i] <= j)
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - c[i]] + m[i]);
-			else
-				dp[i][j] = dp[i - 1][j];
-
-			if (dp[i][j] >= M) result = min(result, j);
-		}
-	}
-	
-
-	cout << result << endl;
-
-	return 0;
+   cout << ans << "\n";
+   return 0;
 }
