76c2e6f17ec27fb69ec340d5f11e7882c950dc09
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-02-21 23:51:27 +0900

    [Silver I] Title: 관중석, Time: 112 ms, Memory: 5932 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.cc
+++ 백준/Silver/10166. 관중석/관중석.cc
@@ -2,28 +2,24 @@
 #include <vector>
 using namespace std;
 int D1, D2, cnt = 0;
-vector<int> arr;
+bool check[2001][2001];
+
+// 최대 공약수
+int gcd(int x, int y) { return y == 0 ? x : gcd(y, x % y); }
 
 int main() {
    ios_base::sync_with_stdio(false);
    cin.tie(0); cout.tie(0);
    cin >> D1 >> D2;
-   arr = vector<int>(D2 + 1);
-   
    for (int i = D1; i <= D2; ++i) {
-      arr[i] = i;
-      if (i != D1) {
-         bool divisor = false;
-         for (int j = D1; j < i; ++j) { // 약수 계산 D1 = 3, i = 6
-            if (i % j == 0) { // 6 % 3 == 0
-               divisor = true; // 약수 하나라도 존재함
-               arr[i] -= arr[j];
-            }
+      for (int j = 1; j <= i; ++j) {
+         int g = gcd(i, j);
+         int x = j / g, y = i / g;
+         if(check[x][y] == 0) {
+            check[x][y] = true;
+            cnt++;
          }
-         // D1 이상의 i의 약수가 존재하지 않을 경우
-         if (!divisor) arr[i] = i - 1;
       }
-      cnt += arr[i];
    }
    cout << cnt;
    return 0;
