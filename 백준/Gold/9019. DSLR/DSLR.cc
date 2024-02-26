d5bf2c81ebb01a909a7b0656a14a5c2a54975f74
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-02-27 02:22:24 +0900

    Update DSLR.cc

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -1,81 +1,65 @@
-#include <queue>
 #include <iostream>
+#include <queue>
 #include <string>
 #include <cstring>
-
 using namespace std;
+int T, A, B;
+bool visited[10000] = {false, };
 
-int a, b;
-bool visited[10000];
+void bfs() {
+   queue<pair<int, string>> q;
+   q.push({A, ""}); // A: 검사할 초기값, "": 출력값, 큐마다 관리할 빈 문자열
+   visited[A] = true;
 
-void bfs()
-{
-    queue<pair<int, string>> q;
-    q.push(make_pair(a, ""));
-    visited[a] = true;
+   while(!q.empty()) {
+      int cur = q.front().first;
+      string ans = q.front().second;
+      q.pop();
+      if (cur == B) { cout << ans << "\n"; return; } // 현재 문자열이 B와 동일할 경우 출력
 
-    while (!q.empty())
-    {
-        int cur_num = q.front().first;
-        string cur_op = q.front().second;
-        q.pop();
-
-        if (cur_num == b)
-        {
-            cout << cur_op << '\n';
-            return;
-        }
-
-        int D, S, L, R, temp;
-        // D 연산
-        D = (cur_num * 2) % 10000;
-        if (!visited[D])
-        {
-            visited[D] = true;
-            q.push(make_pair(D, cur_op + "D"));
-        }
-
-        // S 연산
-        S = cur_num - 1 < 0 ? 9999 : cur_num - 1;
-        if (!visited[S])
-        {
-            visited[S] = true;
-            q.push(make_pair(S, cur_op + "S"));
-        }
-
-        // L 연산
-        L = (cur_num % 1000) * 10 + (cur_num / 1000);
-        if (!visited[L])
-        {
-            visited[L] = true;
-            q.push(make_pair(L, cur_op + "L"));
-        }
-
-        // R 연산
-        R = cur_num / 10 + (cur_num % 10) * 1000;
-        if (!visited[R])
-        {
-            visited[R] = true;
-            q.push(make_pair(R, cur_op + "R"));
-        }
-    }
+      for (int i = 0; i < 4; ++i) { // D: 0, S: 1, L: 2, R: 3
+         int next;
+         if (i == 0) {
+            next = 2 * cur % 10000;
+            if (!visited[next]) {
+               visited[next] = true;
+               q.push({next, ans + "D"});
+            }
+         }
+         else if (i == 1) {
+            next = cur - 1 < 0 ? 9999 : cur - 1;
+            if (!visited[next]) {
+               visited[next] = true;
+               q.push({next, ans + "S"});
+            }
+         }
+         else if (i == 2) {
+            next = cur % 1000 * 10 + cur / 1000;
+            if (!visited[next]) {
+               visited[next] = true;
+               q.push({next, ans + "L"});
+            }
+         }
+         else if (i == 3) {
+            next = cur % 10 * 1000 + cur / 10;
+            if (!visited[next]) {
+               visited[next] = true;
+               q.push({next, ans + "R"});
+            }
+         }
+      }
+   }
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int T;
-    cin >> T;
-
-    while (T--)
-    {
-        cin >> a >> b;
-        memset(visited, false, sizeof(visited)); // 초기화
-        bfs();
-    }
-
-    return 0;
+int main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(0); cout.tie(0);
+   cin >> T;
+   for (int i = 0; i < T; ++i) {
+      memset(visited, false, sizeof(visited));
+      cin >> A >> B;
+      bfs();
+   }
+   
+   return 0;
 }
