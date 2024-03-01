647bd501487daaeab90dc1bece3ea8a6941b4ac9
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-03-01 18:39:39 +0900

    Update 숨바꼭질 3.cc

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -6,29 +6,22 @@ using namespace std;
 int N, K;
 bool visited[100001] = {false, };
 
-void dijkstra() {
+void dijkstra() { // 우선순위 큐
    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-   pq.push({0, N});
+   pq.push({0, N}); // {시간, 위치} 형태로 저장
 
    while (!pq.empty()) {
       int time = pq.top().first;
       int curX = pq.top().second;
       pq.pop();
 
-      if (visited[curX]) continue;
+      if (visited[curX]) continue; // 방문한 위치일 경우 건너뛰기
       visited[curX] = true;
 
-      if (curX == K) {
-         cout << time;
-         return;
-      }
-
-      if (curX - 1 >= 0)
-         pq.push({time + 1, curX - 1});
-      if (curX + 1 <= 100000)
-         pq.push({time + 1, curX + 1});
-      if (curX * 2 <= 100000)
-         pq.push({time, curX * 2});
+      if (curX == K) { cout << time; return; } // 정답 위치에 도달했을 경우 출력
+      if (curX - 1 >= 0) pq.push({time + 1, curX - 1}); // 1초 후 (X - 1)
+      if (curX + 1 <= 100000) pq.push({time + 1, curX + 1}); // 1초 후 (X + 1)
+      if (curX * 2 <= 100000) pq.push({time, curX * 2}); // 0초 후 (2 * X)
    }
 }
 
