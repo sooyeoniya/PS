a88abdce8fd9274299f5b361c1fa204fd865bea4
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-03-25 22:17:23 +0900

    Update 숫자 카드 2.cc

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,3 +1,4 @@
+// 이분탐색: 메모리 3972KB 시간 312ms
 #include <iostream>
 #include <algorithm>
 
@@ -21,4 +22,31 @@ int main() {
 		cin >> card;
 		cout << upper_bound(arr, arr + N, card) - lower_bound(arr, arr + N, card)<<" ";
 	}
+}
+
+// 해시맵: 메모리 43828KB 시간 480ms
+// 풀이 시간: 44m33s83
+// 시간 복잡도: O(N)
+// 공간 복잡도: O(N)
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int N, M;
+unordered_map<int, int> m; // 카드 숫자 num, 해당 숫자 num의 개수
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; ++i) { // 개수 카운트
+		int num; cin >> num;
+		m[num]++;
+	}
+	cin >> M;
+	for (int i = 0; i < M; ++i) { // 바로 출력
+		int num; cin >> num;
+		cout << m[num] << " ";
+	}
+    return 0;
 }
