cfcfbf031e968f70cf33f5674ca8095afd51c07e
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-03-25 22:20:03 +0900

    Update 숫자 카드 2.cc

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,26 +1,26 @@
 // 이분탐색: 메모리 3972KB 시간 312ms
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 int arr[500002];
-int N, M, card;
+int N, M;
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(0); cout.tie(0);
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> card;
-		arr[i] = card;
+	for (int i = 0; i < N; ++i) {
+		int num; cin >> num;
+		arr[i] = num;
 	}
-	sort(arr, arr + N); // 이분탐색을 위해 오름차순 정렬
+    
+	sort(arr, arr + N);
 
 	cin >> M;
-	for (int i = 0; i < M; i++) {
-		cin >> card;
-		cout << upper_bound(arr, arr + N, card) - lower_bound(arr, arr + N, card)<<" ";
+	for (int i = 0; i < M; ++i) {
+		int num; cin >> num;
+		cout << upper_bound(arr, arr + N, num) - lower_bound(arr, arr + N, num) << " ";
 	}
 }
 
