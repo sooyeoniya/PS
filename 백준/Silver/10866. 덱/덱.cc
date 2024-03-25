00aa10bd89b0468faf588ff42b5c5ccb917022db
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-03-25 18:50:49 +0900

    [Silver IV] Title: 덱, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <deque>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int N; cin >> N;
+	deque<int> dq;
+	for (int i = 0; i < N; ++i) {
+		string str; cin >> str;
+		if (str == "push_back" || str == "push_front") {
+			int num; cin >> num;
+			if (str == "push_back") dq.push_back(num);
+			else dq.push_front(num);
+		}
+		else {
+			if (str == "front") {
+				if(!dq.empty()) cout << dq.front() << "\n";
+				else cout << "-1\n";
+			}
+			if (str == "back") {
+				if(!dq.empty()) cout << dq.back() << "\n";
+				else cout << "-1\n";
+			}
+			if (str == "size") cout << dq.size() << "\n";
+			if (str == "empty") {
+				if (dq.empty()) cout << "1\n";
+				else cout << "0\n";
+			}
+			if (str == "pop_front") {
+				if (!dq.empty()) {
+					cout << dq.front() << "\n";
+					dq.pop_front();
+				}
+				else cout << "-1\n";
+			}
+			if (str == "pop_back") {
+				if (!dq.empty()) {
+					cout << dq.back() << "\n";
+					dq.pop_back();
+				}
+				else cout << "-1\n";
+			}
+		}
+	}
+	return 0;
+}
