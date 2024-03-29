e0d85517ce3c43e866ac177fca8ff1824775b085
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-03-30 01:16:04 +0900

    Update 미확인 도착지.cc

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
@@ -76,3 +76,70 @@ int main() {
     }
     return 0;
 }
+
+=======================
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int T, n, m, t, s, g, h;
+vector<bool> candidates; // 목적지 후보
+vector<int> dis; // 각 인덱스까지의 최단거리
+vector<pair<int, int>> graph[2001];
+
+void dijkstra(int start) {
+    dis[start] = 0; // 기준이 되는 출발지 거리 0
+    priority_queue<pair<int, int>> pq;
+    pq.push({0, start}); // 이동거리, 현재 위치
+
+    while(!pq.empty()) {
+        int curDis = -pq.top().first; // 이동거리 (음수화)
+        int curPos = pq.top().second; // 현재 위치
+        pq.pop();
+        for (int i = 0; i < graph[curPos].size(); ++i) { // 인접한 정점의 개수만큼 순회
+            int nDis = curDis + graph[curPos][i].first;
+            int nPos = graph[curPos][i].second;
+            // 현재 위치를 거쳐서 다음 위치로 가는 거리가 더 짧은 경우 최단 경로 갱신
+            if (nDis < dis[nPos]) {
+                dis[nPos] = nDis;
+                pq.push({-nDis, nPos}); // 짧은 이동거리부터 우선순위 큐에 넣어지도록 음수화
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> T;
+    for (int testCase = 0; testCase < T; ++testCase) {
+        cin >> n >> m >> t >> s >> g >> h;
+        // graph 초기화 (size() 함수 사용을 위한 graph 선언 및 초기화 방법)
+        for (int i = 1; i <= n; i++) vector<pair<int, int>>().swap(graph[i]);
+        candidates = vector<bool>(n + 1, false);
+        dis = vector<int>(n + 1, 1e9); // 최단 경로 비교를 위해 무한대로 초기화
+        for (int i = 0; i < m; ++i) {
+            int a, b, d;
+            cin >> a >> b >> d;
+            // g, h 경로는 홀수화, 나머지 짝수화
+            if ((a == g && b == h) || (a == h && b == g)) d = d * 2 - 1;
+            else d = d * 2;
+            // 양방향 도로
+            graph[a].push_back({d, b});
+            graph[b].push_back({d, a});
+        }
+        for (int i = 0; i < t; ++i) {
+            int x; cin >> x;
+            candidates[x] = true;
+        }
+
+        dijkstra(s);
+
+        for (int i = 1; i <= n; ++i) // 홀수일 때만 출력 (오름차순)
+            if (candidates[i] && dis[i] % 2 == 1)
+                cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
