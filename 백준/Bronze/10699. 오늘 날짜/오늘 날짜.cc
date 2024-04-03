3e2371cdb9e59479f0529e802558af7a0b7a8af0
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-04-03 18:50:41 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    time_t timer = time(NULL);
+    struct tm* t = localtime(&timer);
+    cout << t->tm_year + 1900 << "-";
+    cout.width(2); cout.fill('0');
+    cout<< t->tm_mon + 1 << "-";
+    cout.width(2); cout.fill('0');
+    cout << t->tm_mday;
+    return 0;
+}
