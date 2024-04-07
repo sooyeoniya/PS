8f38148550751d148fea7c484f2eda4a82805761
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-04-07 14:11:31 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2552 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,35 @@
+// 풀이 시간: 48m37s05
+// 시간 복잡도: O(n)
+// 공간 복잡도: O(n)
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    int num = 1;
+    stack<int> s;
+    vector<char> ans;
+    for (int i = 0; i < n; ++i) {
+        int x; cin >> x;
+        while (num <= x) {
+			s.push(num);
+			num++;
+			ans.push_back('+');
+		}
+		if (s.top() == x) {
+			s.pop();
+			ans.push_back('-');
+		} else {
+			cout << "NO";
+			return 0;
+		}
+    }
+    for (int i = 0; i < ans.size(); ++i) {
+		cout << ans[i] << "\n";
+	}
+    return 0;
+}
