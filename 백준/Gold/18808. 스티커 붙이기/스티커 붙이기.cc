16d2d62bb86f4e6348c4b0dada41502e9fbeec07
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-04-10 20:52:16 +0900

    [Gold III] Title: 스티커 붙이기, Time: 20 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int N, M, K, R, C;
+bool check = false;
+vector<vector<int>> arr, temp;
+
+void stick() {
+    // 전체 위치 순회하여 붙일 수 있는 공간 찾아 붙이기
+    for (int i = 0; i <= N - R; ++i) { 
+        for (int j = 0; j <= M - C; ++j) {
+            bool flag = true;
+            for (int r = 0; r < R; ++r)
+                for (int c = 0; c < C; ++c) {
+                    if (temp[r][c] == 1 && arr[r + i][c + j] == 1) 
+                        { flag = false; break; } // 이미 스티커가 붙여있을 경우 종료
+                }
+            if (flag) { // 해당 스티커 붙이기
+                check = true;
+                for (int r = 0; r < R; ++r)
+                    for (int c = 0; c < C; ++c)
+                        if (temp[r][c] == 1)
+                            arr[r + i][c + j] = temp[r][c];
+                return;
+            }
+        }
+    }
+}
+
+void rotate() {
+    for (int i = 0; i < 4; ++i) { // 0도, 90도, 180도, 270도
+        stick();
+        if (check) break; // 스티커 붙었을 경우 회전 종료
+        int T = R; R = C; C = T; // R <-> C
+        vector<vector<int>> rotated(R, vector<int>(C));
+        for (int i = 0; i < R; ++i)
+            for (int j = 0; j < C; ++j)
+                rotated[i][C - j - 1] = temp[j][i];
+        temp = rotated;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> N >> M >> K;
+    arr = vector<vector<int>>(N, vector<int>(M, 0));
+    for (int i = 0; i < K; ++i) {
+        cin >> R >> C;
+        temp = vector<vector<int>>(R, vector<int>(C, 0));
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c)
+                cin >> temp[r][c];
+        // 똑바로 해도, 90도 회전해도 스티커가 노트북보다 클 경우 스티커 버림
+        if ((R > N || C > M) && (R > M || C > N)) continue;
+        check = false; // 스티커 부착 여부
+        rotate(); // 스티커 회전
+    }
+    int cnt = 0;
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < M; ++j)
+            if (arr[i][j] == 1) cnt++;
+    cout << cnt;
+    return 0;
+}
