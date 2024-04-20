24e220b7984405e14656ca5b6b4d621a5b989ce1
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-04-20 15:06:22 +0900

    Update solved．ac.cc

--- 백준/Silver/18110. solved．ac/solved．ac.cc
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -1,33 +1,25 @@
+// 풀이 시간: 10m37s48
+// 시간 복잡도: O(n)
+// 공간 복잡도: O(n)
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
 #include <cmath>
 using namespace std;
+vector<int> arr;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int n, res = 0;
-	cin >> n;
-
-	if (n == 0) {
-		cout << res;
-		return 0;
-	}
-
-	vector<int> numbers(n);
-	for (int i = 0; i < n; i++) cin >> numbers[i];
-
-	sort(numbers.begin(), numbers.end());
-	int idx = round(n * 0.15);
-	double sum = 0;
-
-	for (int i = idx; i < n - idx; i++) sum += numbers[i];
-
-	res = round(sum / (n - idx * 2));
-	cout << res;
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    if (n == 0) { cout << "0"; return 0; }
+    arr = vector<int>(n);
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    int num = round(n * 0.15);
+    double sum = 0;
+    for (int i = num; i < n - num; ++i) sum += arr[i];
+    int avg = round(sum / (n - 2 * num));
+    cout << avg;
+    return 0;
 }
