306bbae0bef80f17111212dbc0b4e4fb222cfd5a
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-04-24 10:00:15 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int N; cin >> N;
+    cout << N - 1946;
+    return 0;
+}
