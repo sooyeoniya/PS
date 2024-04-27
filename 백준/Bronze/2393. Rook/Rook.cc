abe5888bd6f5f0f63de419d7b0a9f91fb8ea4b20
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-04-27 15:08:30 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << "  ___  ___  ___\n";
+    cout << "  | |__| |__| |\n";
+    cout << "  |           |\n";
+    cout << "   \\_________/\n";
+    cout << "    \\_______/\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |_____|\n";
+    cout << "  __/       \\__\n";
+    cout << " /             \\\n";
+    cout << "/_______________\\\n";
+    return 0;
+}
