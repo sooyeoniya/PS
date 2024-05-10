928c9b0fdb862107ee17ac6751ab41d5c8deb94e
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-05-10 16:43:31 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int N; cin >> N;
+    vector<int> dp(N + 1, 0);
+    dp[1] = 0; // 연산 횟수 저장
+    for (int i = 2; i <= N; ++i) {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+        if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+    }
+    cout << dp[N];
+    return 0;
+}
