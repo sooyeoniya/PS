54f43e202cd6946b7ec9e375288e18347dee016c
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-05-12 21:08:44 +0900

    [Gold IV] Title: 가희와 여행가요, Time: 116 ms, Memory: 12148 KB -BaekjoonHub

--- 백준/Gold/30797. 가희와 여행가요/가희와 여행가요.cc
+++ 백준/Gold/30797. 가희와 여행가요/가희와 여행가요.cc
@@ -1,67 +1,66 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <vector>
 using namespace std;
+int N, Q;
+long long tAns = 0, cAns = 0; // 타입 변경
+int parent[200001] = { 0, };
+vector<pair<pair<long long, long long>, pair<int, int>>> arr; // 타입 변경
 
-struct EDGE {
-    int From, To;
-    long long Cost, Time;
-};
-
-int N, Q, From, To;
-long long Cost, Time, Answer_Time = 0, Answer_Cost = 0;
-vector<EDGE> Edge;
-int Parent[200001];
-
-bool Comp(EDGE A, EDGE B) {
-    if (A.Cost == B.Cost) {
-        return (A.Time < B.Time);
-    }
-    return (A.Cost < B.Cost);
+// 부모노드 찾기
+int getParent(int node) {
+    if (node == parent[node]) return node;
+    else return parent[node] = getParent(parent[node]);
 }
 
-int find_Parent(int X) {
-    if (Parent[X] == X) return X;
-    return Parent[X] = find_Parent(Parent[X]);
+// 부모 합치기
+void unionParent(int node1, int node2) {
+    node1 = getParent(node1);
+    node2 = getParent(node2);
+    if (node1 < node2) parent[node2] = node1;
+    else parent[node1] = node2;
 }
 
-void union_Group(int X, int Y) {
-    int ParentX = find_Parent(X);
-    int ParentY = find_Parent(Y);
-    if (ParentX < ParentY) {
-        Parent[ParentY] = ParentX;
-    }
-    else {
-        Parent[ParentX] = ParentY;
-    }
+// 사이클 판단
+bool isCycle(int node1, int node2) {
+    node1 = getParent(node1);
+    node2 = getParent(node2);
+    if (node1 == node2) return true;
+    else return false;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    for (int i = 0; i < 200001; ++i) Parent[i] = i;
     cin >> N >> Q;
-    for (int i = 0; i < Q; i++) {
-        cin >> From >> To >> Cost >> Time;
-        Edge.push_back({ From, To, Cost, Time });
-    }
-    sort(Edge.begin(), Edge.end(), Comp);
+    for (int i = 1; i <= N; ++i) parent[i] = i; // 부모 입력
 
-    for (int i = 0; i < (int)Edge.size(); i++) {
-        if (find_Parent(Edge[i].From) != find_Parent(Edge[i].To)) {
-            union_Group(Edge[i].From, Edge[i].To);
-            Answer_Cost += Edge[i].Cost;
-            Answer_Time = max(Answer_Time, Edge[i].Time);
+    for (int i = 0; i < Q; ++i) { // 노선에 대한 시간 및 비용 입력
+        int from, to;
+        long long cost, time;
+        cin >> from >> to >> cost >> time;
+        arr.push_back({{cost, time}, {from, to}});
+    }
+    
+    sort(arr.begin(), arr.end()); // 정렬
+
+    for (int i = 0; i < arr.size(); ++i) {
+        if (!isCycle(arr[i].second.first, arr[i].second.second)) {
+            unionParent(arr[i].second.first, arr[i].second.second);
+            cAns += arr[i].first.first;
+            tAns = max(tAns, arr[i].first.second);
         }
     }
 
-    for (int i = 2; i <= N; i++) {
-        if (find_Parent(i) != 1) {
-            cout << "-1\n";
-            return 0;
+    bool connected = true;
+    int rootParent = getParent(1); // 가희의 부모 노드(1임)
+    for (int i = 2; i <= N; ++i) { // 나머지 노드들 탐색하여 가희와 연결되었는지 확인
+        if (getParent(i) != rootParent) {
+            connected = false;
+            break;
         }
     }
-    cout << Answer_Time << " " << Answer_Cost << "\n";
+    if (!connected) cout << "-1"; // 가희와 연결되지 않은 경우
+    else cout << tAns << " " << cAns;
     return 0;
 }
