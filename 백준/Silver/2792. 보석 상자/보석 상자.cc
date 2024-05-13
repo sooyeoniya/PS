79f3a473a501623669b3fd69733fe012e6383b70
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-05-13 22:39:55 +0900

    [Silver I] Title: 보석 상자, Time: 80 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.cc
@@ -0,0 +1,31 @@
+// 풀이 시간: 54m06s75
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int N, M, ans;
+int gem[300000] = { 0, };
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < M; ++i) cin >> gem[i];
+    sort(gem, gem + M);
+
+    int l = 1, r = gem[M - 1], m; // 질투심 값
+    while (l <= r) {
+        m = (l + r) / 2;
+        int cnt = 0;
+        for (int i = 0; i < M; ++i) {
+            cnt += gem[i] / m; // 최대 m(질투심 개수)만큼 가져감
+            if (gem[i] % m) cnt++; // 보석 나머지가 생기면 한명 더 나누어 주어야 함
+        }
+        if (cnt <= N) { // cnt가 N보다 작거나 같은 경우
+            r = m - 1; 
+            ans = m; 
+        }
+        else l = m + 1; 
+    }
+    cout << ans;
+    return 0;
+}
