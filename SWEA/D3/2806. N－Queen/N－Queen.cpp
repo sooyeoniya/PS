0724cd0caef345cc4852e138c04e1cd155d55a54
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-05-17 21:36:51 +0900

    [D3] Title: N-Queen, Time: 23 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.cpp
+++ SWEA/D3/2806. N－Queen/N－Queen.cpp
@@ -4,6 +4,7 @@
 // https://tear94fall.github.io/lecture/2020/09/16/n-queen-problem.html
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 #define MAX 10
 using namespace std;
 int N, ans = 0;
@@ -11,18 +12,21 @@ int arr[MAX] = { 0, };
 
 // dfs
 void nqueen(int num) {
-	if (num == N) {
+	if (num == N) { // N개의 퀸이 전부 배치됨
 		ans++; return;
 	}
 	for (int i = 0; i < N; ++i) {
-		arr[num] = i;
-		bool check = true;
+		arr[num] = i; // 퀸 위치: (num번째 열, i번째 행)
+		bool check = true; // 퀸 배치 가능 여부
 		for (int j = 0; j < num; ++j)
-			if (arr[j] == arr[num] || abs(arr[j] - arr[num]) == abs(j - num)) {
+			// 같은 행에 다른 퀸이 있는지 확인
+			if (arr[j] == arr[num] 
+				// 대각선에 다른 퀸이 있는지 확인 (열의 차이 == 행의 차이)
+				|| abs(arr[j] - arr[num]) == abs(j - num)) {
 				check = false; 
 				break;
 			}
-		if (check) nqueen(num + 1);
+		if (check) nqueen(num + 1); // 다음 퀸 배치
 	}
 }
 
@@ -32,8 +36,9 @@ int main() {
 	int T; cin >> T;
 	for (int test_case = 1; test_case <= T; ++test_case) {
 		cin >> N; 
+		memset(arr, 0, sizeof(arr)); // 매번 초기화
 		ans = 0;
-		nqueen(0);
+		nqueen(0); // 0번째 열부터 시작
 		cout << "#" << test_case << " " << ans << "\n";
 	}
 	return 0;
