eb6119cc00758376f5195123855a6d57741cce71
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-05-24 01:42:42 +0900

    Update 최소 힙.cc

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -11,7 +11,7 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    priority_queue<int, vector<int>, greater<int>> pq;
+    priority_queue<int, vector<int>, greater<int>> pq; // 작은 값 순으로 정렬
     int N; cin >> N;
     for (int i = 0; i < N; ++i) {
         int x; cin >> x;
