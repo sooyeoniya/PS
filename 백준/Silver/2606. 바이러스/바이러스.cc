f59fec12e1d703b55d494ede8ca66f2eb96dafa9
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-05-24 00:09:32 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -0,0 +1,49 @@
+// 풀이 시간: 11m42s82(bfs)
+// 시간 복잡도: O(N)
+// 공간 복잡도: O(NM) // 최대 시간복잡도
+
+// <풀이 과정>
+// 연결리스트로 간선 저장
+// 1번 컴퓨터부터 큐에 넣고 1번 컴퓨터와 연결된 다른 컴퓨터를 연결리스트에서 찾아서 이동
+// 오답 -> 양방향 간선 연결
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int N, M, ans = 0;
+vector<vector<int>> arr;
+vector<int> visited;
+
+void bfs() {
+    queue<int> q;
+    q.push(1);
+    visited[1] = true;
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for (int i = 0; i < arr[cur].size(); ++i) {
+            int next = arr[cur][i];
+            if (!visited[next]) {
+                visited[next] = true;
+                q.push(next);
+                ans++;
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    arr = vector<vector<int>>(N + 1);
+    visited = vector<int>(N + 1, false);
+    for (int i = 0; i < M; ++i) {
+        int a, b; cin >> a >> b;
+        arr[a].push_back(b);
+        arr[b].push_back(a); // 양방향 연결
+    }
+    bfs();
+    cout << ans;
+    return 0;
+}
