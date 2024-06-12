6543522a98a43ab122fe125dc80099cec4944323
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-06-12 21:44:40 +0900

    Update 원숭이 스포츠.cc

--- 백준/Gold/16438. 원숭이 스포츠/원숭이 스포츠.cc
+++ 백준/Gold/16438. 원숭이 스포츠/원숭이 스포츠.cc
@@ -9,17 +9,17 @@ int N, arr[7][99]; // arr[day][N];
 void match(int st, int ed, int team, int day) {
     if (day == 7) return; // 7일 되면 종료
     int mid = (st + ed) / 2;
-    for (int i = st; i <= ed; ++i) // mid 기준 분할
-        arr[day][i] = i <= mid ? team : !team;
+    for (int i = st; i < ed; ++i) // mid 기준 분할
+        arr[day][i] = i < mid ? team : !team;
     match(st, mid, !team, day + 1);
-    match(mid + 1, ed, team, day + 1);
+    match(mid, ed, team, day + 1);
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     cin >> N;
-    match(0, N - 1, 0, 0);
+    match(0, N, 0, 0);
     for (int i = 0; i < 7; ++i) {
         for (int j = 0; j < N; ++j) {
             // arr 값에 따라 두 팀으로 분할
