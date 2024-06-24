f548557371d19e787abe94be9e35da8464c6d43c
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-06-24 22:09:12 +0900

    Update 가희와 클럽 오디션 1.java

--- 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.java
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.java
@@ -1,3 +1,4 @@
+// BufferedReader/BufferedWriter
 import java.io.*;
 import java.util.*;
 
@@ -31,4 +32,26 @@ public class Main {
 		bw.flush();
 		bw.close();
 	}
+}
+
+
+// Scanner
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lv = sc.nextInt();
+        String str = sc.next();
+        sc.close();
+        int score;
+        switch (str) {
+            case "bad": score = lv * 200; break;
+            case "cool": score = lv * 400; break;
+            case "great": score = lv * 600; break;
+            case "perfect": score = lv * 1000; break;
+            default: score = 0;
+        }
+        System.out.print(score);
+    }
 }
