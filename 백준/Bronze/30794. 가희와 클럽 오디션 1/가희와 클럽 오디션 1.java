7b9f35ffcf04ec346d16707ff78418ee20a11ff9
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-06-24 22:06:19 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 132 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.java
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.java
@@ -1,19 +1,34 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int lv = sc.nextInt();
-        String str = sc.next();
-        sc.close();
-        int score;
-        switch (str) {
-            case "bad": score = lv * 200; break;
-            case "cool": score = lv * 400; break;
-            case "great": score = lv * 600; break;
-            case "perfect": score = lv * 1000; break;
-            default: score = 0;
-        }
-        System.out.print(score);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int lv = Integer.parseInt(st.nextToken());
+		
+		switch(st.nextToken()) {
+			case "miss":
+				bw.write(String.valueOf(0));
+				break;
+			case "bad":
+				bw.write(String.valueOf(lv * 200));
+				break;
+			case "cool":
+				bw.write(String.valueOf(lv * 400));
+				break;
+			case "great":
+				bw.write(String.valueOf(lv * 600));
+				break;
+			case "perfect":
+				bw.write(String.valueOf(lv * 1000));
+				break;
+			default:
+				break;
+		}
+		bw.flush();
+		bw.close();
+	}
 }
