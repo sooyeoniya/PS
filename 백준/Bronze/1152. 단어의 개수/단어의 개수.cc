a8b5f85a7871c25f82090e65554e53be4b34f150
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2024-09-27 21:04:06 +0900

    [Bronze II] Title: 단어의 개수, Time: 4 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -1,21 +1,19 @@
-#include <stdio.h>
-#include <string.h>
-#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main(void)
-{
-	char a[1000001];
-	int i = 0, sum = 0;
-	fgets(a, sizeof(a), stdin);
-	if (a[0] == ' ')
-		sum--;
-
-		do {
-			if (a[i] == ' ' && a[i + 1] != '\n')
-				sum++;
-			i++;
-		} while (a[i] != '\0');
-			sum++;
-			printf("%d", sum);
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	
+	string s;
+	getline(cin, s);
+	
+	if (s.length() == 1 && s[0] == ' ') { cout << 0; return 0; }
+	
+	int ans = 1;
+	for (int i = 1; i < s.length() - 1; ++i) if (s[i] == ' ') ans++;
+	cout << ans;
+	
 	return 0;
 }
