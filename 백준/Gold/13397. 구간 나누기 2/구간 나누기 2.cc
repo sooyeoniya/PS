aff9ceffd7ee64a4ca6d097d7d8f169b2b5edfd2
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-01-02 03:19:38 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.cc
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.cc
@@ -1,26 +1,21 @@
-// 풀이 시간: 3:25:29
-// 시간 복잡도: O(nlogS) - S는 배열의 합(sum)을 의미, for문을 통해 0 ~ n까지 선형 탐색
-// 공간 복잡도: O(n) - n은 배열의 크기
-// 참고 코드: https://leesh111112.tistory.com/356
-//            https://ohcode.tistory.com/17
-//            https://8156217.tistory.com/46
 #include <iostream>
-#include <limits.h> // INT_MAX 사용
+#include <limits.h>
 using namespace std;
 
-int n, m, A[5000]; // 배열 크기 n, 배열을 m개 이하의 구간으로 나눔, 배열 A의 최대 n의 크기는 5000
-int sum = 0, ans = INT_MAX; // 무한대 정의, 대략 21억
+// https://leesh111112.tistory.com/356
+// https://ohcode.tistory.com/17
+
+int n, m, A[5000];
+int sum = 0, ans = INT_MAX; // https://8156217.tistory.com/46
 
 bool check(int mid) { // 주어진 mid 값을 기준으로 배열을 m개 이하 구간으로 나눌 수 있는지 확인(mid 값이 유효한지 판단)
-	int count = 1, maxX = A[0], minX = A[0]; // count를 1(첫 번째 구간), maxX와 minX를 첫 번째 원소 값으로 설정
-	for (int i = 0; i < n; i++) { // 배열 전체 순회
-		// 현재 구간의 최댓값과 최솟값 구하기
-		maxX = max(A[i], maxX); 
+	int count = 1, maxX = A[0], minX = A[0];
+	for (int i = 0; i < n; i++) {
+		maxX = max(A[i], maxX);
 		minX = min(A[i], minX);
 
-		// 만약 현재 구간의 '구간의 최댓값'(최댓값-최솟값)이
-		// 현재의 구간 점수 최댓값(mid)보다 크면 새로운 구간으로 시작
-		// 왜냐하면 구간 점수의 최댓값을 최소로 하는 배열의 분할 방법을 찾는 것이기 때문
+		// 만약 현재 구간의 구간의 최댓값(최댓값-최솟값)이 현재의 구간 점수 최댓값보다 크면 새로운 구간으로 시작
+		// 왜냐하면 구간 점수 최댓값의 최솟값을 구하는 것이기 때문에
 		if (maxX - minX > mid) {
 			count++; // 새로운 구간 시작
 			maxX = minX = A[i];
@@ -33,12 +28,10 @@ bool check(int mid) { // 주어진 mid 값을 기준으로 배열을 m개 이하
 	// 그 반대의 경우에는 mid가 너무 크거나 적당하다는 뜻이므로 true를 반환하여 mid 범위를 감소시키거나 유지하면서 최솟값 갱신
 	return m >= count;
 }
-
 int binary_search() { // 구간 점수 최댓값 이진 탐색
-	// 구간 점수 최댓값(배열에 있는 수의 구간 별 최댓값-최솟값 중 최댓값)은 sum을 절대 넘지 않음
-	int mid, left = 0, right = sum;
+	int mid, left = 0, right = sum; // 구간 점수 최댓값(배열에 있는 수의 구간 별 최댓값-최솟값 중 최댓값)은 sum을 절대 넘지 않음
 	while (left <= right) {
-		mid = (left + right) / 2; // 임의의 구간 점수 최댓값 설정
+		mid = (left + right) / 2;
 		if (check(mid)) {
 			right = mid - 1;
 			if (mid < ans) ans = mid; // mid의 값(구간 점수 최댓값)이 현재 구간 점수의 최댓값보다 작을 경우 갱신
@@ -56,7 +49,7 @@ int main(void) {
 
 	for (int i = 0; i < n; i++) {
 		cin >> A[i];
-		sum += A[i]; // 배열의 모든 원소의 합
+		sum += A[i];
 	}
 	cout << binary_search();
 }
