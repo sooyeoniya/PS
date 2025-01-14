9fb94d63cd2c798b0078ec1f7457cd3bbd75033c
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-01-14 21:35:06 +0900

    [Silver II] Title: 과일 탕후루, Time: 184 ms, Memory: 15236 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.js
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.js
@@ -1,26 +1,28 @@
+// 슬라이딩 윈도우 기법 + 투 포인터
 const inputs = require('fs').readFileSync('/dev/stdin');
 const [n, fruits] = inputs.toString().trim().split('\n');
 const fruitsArr = fruits.split(' ').map(Number);
+let frequencyArr = Array(10).fill(0);
+let low = 0, ans = 0, dNums = 0;
 
-const solution = (i, j) => {
-  let maxNum = 0, cnt = 0;
-  for (let idx = 0; idx < n; ++idx) {
-    if (fruitsArr[idx] !== i && fruitsArr[idx] !== j) {
-      maxNum = Math.max(maxNum, cnt);
-      cnt = 0;
-    } else {
-      cnt += 1;
+for (let high = 0; high < n; ++high) {
+  frequencyArr[fruitsArr[high]] += 1;
+
+  if (frequencyArr[fruitsArr[high]] === 1) {
+    dNums += 1;
+  }
+
+  while (dNums > 2) {
+    frequencyArr[fruitsArr[low]] -= 1;
+
+    if (frequencyArr[fruitsArr[low]] === 0) {
+      dNums -= 1;
     }
-  }
-  return Math.max(maxNum, cnt);
-}
 
-let ans = 0;
-
-for (let i = 1; i <= 9; ++i) {
-  for (let j = i + 1; j <= 9; ++j) {
-    ans = Math.max(ans, solution(i, j));
+    low += 1;
   }
+
+  ans = Math.max(ans, high - low + 1);
 }
 
 console.log(ans);
