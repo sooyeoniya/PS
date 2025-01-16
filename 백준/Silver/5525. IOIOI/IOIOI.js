680df066c6164ea6a6e6d66facb21fdaa02d5aaf
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-01-16 20:29:27 +0900

    [Silver I] Title: IOIOI, Time: 204 ms, Memory: 15676 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'BOJ/input.txt';
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.trim());
+
+const [N, M, S] = input;
+
+function solution() {
+  let cnt = 0;
+  let answer = 0;
+  let i = 0;
+
+  while (i < M - 1) {
+    if (S[i] === 'I' && S.slice(i, i + 3) === 'IOI') {
+      cnt += 1;
+      if (cnt >= N) {
+        answer += 1;
+      }
+      i += 2;
+    } else {
+      cnt = 0;
+      i += 1;
+    }
+  }
+
+  console.log(answer);
+}
+
+solution();
