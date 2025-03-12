0c636e0febd43d97ba90987e18a9ea650b2b3861
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-03-12 23:34:28 +0900

    [Gold V] Title: LCS, Time: 168 ms, Memory: 10116 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.js
+++ 백준/Gold/9251. LCS/LCS.js
@@ -7,24 +7,25 @@ const inputs = fs
   .split("\n")
   .map((el) => el.trim());
 
-function solution(inputs) {
-  const str1 = inputs[0];
-  const str2 = inputs[1];
+function solution2(inputs) {
+  const [str1, str2] = inputs;
 
-  const dp = Array.from(Array(str1.length + 1), () =>
-    Array(str2.length + 1).fill(0)
-  );
+  const dp = new Array(str1.length + 1).fill(0);
 
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      }
+  for (ch of str2) {
+    let prevNum = 0;
+    let curNum = 0;
+
+    for (let j = 1; j < dp.length; j++) {
+      curNum = dp[j];
+
+      if (ch === str1[j - 1]) dp[j] = prevNum + 1;
+      else dp[j] = Math.max(dp[j], dp[j - 1]);
+
+      prevNum = curNum;
     }
   }
-  console.log(dp[str1.length][str2.length]);
+  console.log(dp[dp.length - 1]);
 }
 
-solution(inputs);
+solution2(inputs);
