4ba44bd26cb3fd49d51c4e556e637b04701a593b
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-03-23 14:17:13 +0900

    Update 올바른 괄호.js

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,25 +1,38 @@
-function solution(s){
-    var answer = true;
+// 내 풀이
+function solution(s) {
+  var answer = true;
 
-    let stack = [];
-    
-    for (const c of s) {
-        if (stack.length === 0) {
-            stack.push(c);
-        } else {
-            if (c === '(') {
-                stack.push(c);
-            } else if (c === ')') {
-                if (stack[stack.length - 1] === '(') {
-                    stack.pop(c);
-                } else if (stack[stack.length - 1] === ')') {
-                    stack.push(c);
-                }
-            }
+  let stack = [];
+
+  for (const c of s) {
+    if (stack.length === 0) {
+      stack.push(c);
+    } else {
+      if (c === "(") {
+        stack.push(c);
+      } else if (c === ")") {
+        if (stack[stack.length - 1] === "(") {
+          stack.pop(c);
+        } else if (stack[stack.length - 1] === ")") {
+          stack.push(c);
         }
+      }
     }
+  }
 
-    if (stack.length !== 0) answer = false;
+  if (stack.length !== 0) answer = false;
 
-    return answer;
+  return answer;
+}
+
+// 다른 사람 풀이
+function solution(s) {
+  let count = 0;
+
+  for (const c of s) {
+    count += c === "(" ? 1 : -1;
+    if (count < 0) return false;
+  }
+
+  return count === 0;
 }
