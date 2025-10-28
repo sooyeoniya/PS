4176aa2bb0da4904ffff5f4f9985c2c15fab70ae
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-10-28 21:26:09 +0900

    [Silver III] Title: 게임, Time: 96 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim());
+
+const [X, Y] = input[0].split(" ").map(Number);
+const Z = Math.floor((100 * Y) / X);
+
+let left = 1;
+let right = 1_000_000_000;
+let count = Infinity;
+
+while (left <= right) {
+  const mid = parseInt((left + right) / 2);
+  const newZ = Math.floor((100 * (Y + mid)) / (X + mid));
+
+  if (newZ !== Z) {
+    count = Math.min(count, mid);
+    right = mid - 1;
+  } else {
+    left = mid + 1;
+  }
+}
+
+if (count === Infinity) console.log(-1);
+else console.log(count);
