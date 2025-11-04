2a1babd3395adef9a5c969eab70be0ea562c0f6b
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-11-04 13:10:55 +0900

    Update 게임 맵 최단거리.js

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,4 +1,51 @@
+/**
+* shift() 사용 버전
+*/
+const dx = [1, -1, 0, 0];
+const dy = [0, 0, 1, -1];
 
+const startX = 0;
+const startY = 0;
+const startCnt = 1;
+
+let answer = Infinity;
+const queue = [];
+
+function solution(maps) {
+    const n = maps.length;
+    const m = maps[0].length;
+    
+    const visited = Array.from({ length: n }, () => Array(m).fill(false));
+    queue.push([startX, startY, startCnt]);
+    visited[startX][startY] = true;
+    
+    while (queue.length) {
+        const [curX, curY, cnt] = queue.shift();
+        
+        if (curX === n - 1 && curY === m - 1) {
+            answer = Math.min(answer, cnt);
+            break;
+        }
+        
+        for (let [x, y] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
+            const nextX = curX + x;
+            const nextY = curY + y;
+            
+            if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
+            if (visited[nextX][nextY] || !maps[nextX][nextY]) continue;
+
+            queue.push([nextX, nextY, cnt + 1]);
+            visited[nextX][nextY] = true; // 유의사항: visited를 queue에 삽입하자마자 실행할 것.
+        }
+    }
+    
+    if (answer !== Infinity) return answer;
+    return -1;
+}
+
+/**
+* Queue 직접 구현 버전
+*/
 class Queue {
     constructor () {
         this.list = [];
