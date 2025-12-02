d34b63543b0e4b549753395c030d4ff2dbb5dac2
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-12-02 16:03:53 +0900

    [level 3] Title: 네트워크, Time: 0.60 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,24 +1,22 @@
-let answer = 0;
-
-function solution(n, computers) {    
+function solution(n, computers) {
+    var answer = 0;
     const visited = Array(n).fill(false);
     
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < computers.length; i++) {
         if (!visited[i]) {
-            reculsive(n, i, computers, visited);
+            reculsive(i, n, computers, visited);
             answer += 1;
         }
     }
-    
+
     return answer;
 }
 
-function reculsive(n, node, computers, visited) {
-    visited[node] = true;
-    
+function reculsive(node, n, computers, visited) {    
     for (let i = 0; i < n; i++) {
-        if ((computers[node][i] === 1 || computers[i][node]) && !visited[i]) {
-            reculsive(n, i, computers, visited)
+        if ((computers[node][i] === 1 || computers[i][node] === 1) && !visited[i]) {
+            visited[i] = true;
+            reculsive(i, n, computers, visited);
         }
     }
 }
